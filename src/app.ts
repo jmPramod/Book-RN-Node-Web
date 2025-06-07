c319e3bda537da513f385a840ce6cd213ebeff30
Pramod <pramodjm4@gmail.com>
2025-06-07 16:58:22 +0530

    Added the headers on top

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express from "express"
-import cookieParser from 'cookie-parser';
-import cors from "cors"
-import helmet  from "helmet";
-import morgan from "morgan"
-import hbs, { engine } from "express-handlebars"
-import flash from 'connect-flash';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import hbs, { engine } from "express-handlebars";
+import flash from "connect-flash";
 // import swaggerUI from "swagger-ui-express"
 import "dotenv/config";
 import { adminRoutesMono } from "./routes/monoArchiRoute/auth.mono.routes";
@@ -13,43 +13,37 @@ import { connectMongooseDB } from "./config/db.connect";
 import { bookRoutesRest } from "./routes/restRoutes/book.rest.routes";
 import { ErrorHandelingMiddlewear } from "./middlewear/global.middlewear";
 
-
-
-
 const app = express();
 const runServer = async () => {
-    //!Middlewear 
-    // Helmet for secure headers
-    app.use(helmet());
-  app.use(cors({origin: true, credentials: true}));
-    app.use(express.urlencoded({ extended: true }));// used to parse incoming requests with URL-encoded payloads
-    app.use(express.json());//parses incoming requests with JSON payloads
-    app.use(morgan("dev"));
+  //!Middlewear
+  // Helmet for secure headers
+  app.use(express.urlencoded({ extended: true })); // used to parse incoming requests with URL-encoded payloads
+  app.use(express.json()); //parses incoming requests with JSON payloads
+  app.use(helmet());
+  app.use(cors({ origin: true, credentials: true }));
+  app.use(morgan("dev"));
 
-    // Cookie parser and flash
-    app.use(cookieParser());
-    app.use(flash());
-    
- 
-    // Set up Handlebars view engine
-    app.engine('handlebars', engine());
-    app.set('view engine', 'handlebars');
-    app.set('views', __dirname+'/views');
-    app.use(express.static(__dirname + "/public"));
+  // Cookie parser and flash
+  app.use(cookieParser());
+  app.use(flash());
 
-    app.get("/",(req,res)=>{
-        res.send("Hai")
-    })
-    //REST Routes
-    app.use("/api",authRoutesRest)
-    app.use("/",bookRoutesRest)
+  // Set up Handlebars view engine
+  app.engine("handlebars", engine());
+  app.set("view engine", "handlebars");
+  app.set("views", __dirname + "/views");
+  app.use(express.static(__dirname + "/public"));
 
-    //Admin  Routes
-    app.use("/admin",adminRoutesMono)
-    app.use(ErrorHandelingMiddlewear)
-    connectMongooseDB()
-}
+  app.get("/", (req, res) => {
+    res.send("API working");
+  });
+  //REST Routes
+  app.use("/api", authRoutesRest);
+  app.use("/", bookRoutesRest);
 
-
+  //Admin  Routes
+  app.use("/admin", adminRoutesMono);
+  app.use(ErrorHandelingMiddlewear);
+  connectMongooseDB();
+};
 
 export { runServer, app };
